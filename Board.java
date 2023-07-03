57d4ec0309ec32646eb670fbfb3c1e90958e7cd4
roman <137758436+romptrc@users.noreply.github.com>
2023-07-03 08:51:15 +0200

    v0307

--- Board.java
+++ Board.java
@@ -12,18 +12,18 @@ public class Board{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1600, 845);
         frame.setLocation(100, 100);
-        ImageIcon image = new ImageIcon("schach2906/images/frame_icon.jpg");
+        ImageIcon image = new ImageIcon("\\\\dc1.asg.schule\\home\\ROM.PETRC\\Desktop\\Inf\\Q11\\schach2906\\images\\");
         frame.setIconImage(image.getImage());
         frame.setResizable(false);
         frame.setLayout(null);
 
-        JLabel king_black = new JLabel(new ImageIcon("schach2906/images/king_white.png"));
+        JLabel king_black = new JLabel(new ImageIcon("\\\\\\\\dc1.asg.schule\\\\home\\\\ROM.PETRC\\\\Desktop\\\\Inf\\\\Q11\\\\schach2906\\\\images\\\\king_white.png"));
         king_black.setBounds(350, 0, 90 ,90 );
 
         // Brett als Label displayen
         cboard = new JLabel();
-        cboard.setIcon(new ImageIcon("schach2906/images/chessboard.png"));
-        cboard.setBounds(350,0,700,700);
+        cboard.setIcon(new ImageIcon("\\\\dc1.asg.schule\\home\\ROM.PETRC\\Desktop\\Inf\\Q11\\schach2906\\images\\chessboard.png"));
+        cboard.setBounds(350,0,800,800);
 
         frame.add(king_black);
         frame.add(cboard);
