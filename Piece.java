e107ece2867f3f9953d9b6927282d57140830bf3
roman <137758436+romptrc@users.noreply.github.com>
2023-07-04 13:09:55 +0200

    Commit 0407

---
+++ Piece.java
@@ -0,0 +1,13 @@
+import javax.swing.ImageIcon;
+public abstract class Piece {
+    public ImageIcon image;
+    public int posx,posy;
+    
+
+    public abstract int fwd();
+    
+
+    public void swL(){
+        
+    }
+}
