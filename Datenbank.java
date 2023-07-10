1b23908c8b289f5167b541994aa104669b46227a
JuiRM <138024458+JuiRM@users.noreply.github.com>
2023-07-10 08:47:27 +0200

    Add files via upload

--- Datenbank.java
+++ Datenbank.java
@@ -2,7 +2,7 @@
 
 @author Nick
 
-@version 1007
+@version 0607
 
  */
 
@@ -45,9 +45,7 @@ public class Datenbank {
         } 
     }
     
-    public void datenEinfuegen(Connection connection, String text){
-        
-        try{
+    public void insertDataIntoTable(Connection connection, String text) throws SQLException {
         // SQL-Abfrage zum Einfügen von Daten
         String insertQuery = "INSERT INTO zuege (Zugnummer, Zug) VALUES (?, ?)";
 
@@ -55,7 +53,7 @@ public class Datenbank {
         PreparedStatement statement = connection.prepareStatement(insertQuery);
 
         // Werte für die Parameter festlegen
-        statement.setInt(1, zug+1);
+        statement.setInt(1, zug);
         statement.setString(2, text);
 
         // Einfügen der Daten ausführen
@@ -63,49 +61,13 @@ public class Datenbank {
         
         // Prepared Statement schließen
         statement.close();
-        } catch (SQLException e) {
-          e.printStackTrace();
-        }
     }
-
-    public void tabelleLeeren(Connection connection){
-        
-        try{
+    
+    public static void clearTable(Connection connection) throws SQLException {
         //leert die komplette Tabelle
         String deleteQuery = "DELETE FROM zuege";
         Statement statement = connection.createStatement();
         statement.executeUpdate(deleteQuery);
         statement.close();
-        
-        //Züge auf 0 setzen und die Standart Board-Position einfügen
-        zug = 0;
-        datenEinfuegen(connection, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-        
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public int letzterZug(Connection connection){
-        
-        try{
-        //abfragen des Wertes des letzten Eintrags der Spalte "Zug"
-        String selectQuery = "SELECT MAX(Zug) FROM zuege";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(selectQuery);
-        int letzterZug = resultSet.getInt(1);
-        resultSet.close();
-        statement.close();
-        return letzterZug;
-        } catch(SQLException e){
-            e.printStackTrace();
-            return -1;
-        }
-    }
-
-    public Datenbank(){
-        datenbankErstellen();
-        tabelleErstellen();
-        zug = letzterZug(connection);
     }
 }
