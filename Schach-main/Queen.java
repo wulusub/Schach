cc6a0a2b63858cb53a8f1ea545707ada2a8fb8d6
soko003 <138104130+soko003@users.noreply.github.com>
2023-07-10 09:23:17 +0200

    v1007

--- Schach-main/Queen.java
+++ Schach-main/Queen.java
@@ -1,8 +1,8 @@
 /*
 
-@author Robert
+@author Robert, Sönke
 
-@version 0407
+@version 1007
 
  */
 
@@ -92,6 +92,86 @@ public class Queen extends King {
             System.out.println(ex.getMessage());
         }
         return diRL();
+    }
+
+ //Bewegen nach Links-Oben
+    public int[] left_up(int distance){
+        if(posx-distance*80 > 400 && posy-distance*80 >0){
+            diLU();
+            return new int[]{posx-distance*80, posy-distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Links-Seite
+    public int[] left_side(int distance){
+        if(posx-distance*80 > 400){
+            swl();
+            return new int[]{posx-distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Links-Unten
+     public int[] left_low(int distance){
+        if(posx-distance*80 > 400 && posy+distance*80 < 800){
+            diLL();
+            return new int[]{posx-distance*80, posy+distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Rechts-Oben
+    public int[] right_up(int distance){
+        if(posx+distance*80 < 800 && posy-distance*80 >0){
+            diRU();
+            return new int[]{posx+distance*80, posy-distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Rechts-Seite
+    public int[] right_side(int distance){
+        if(posx+distance*80 < 800){
+            swr();
+            return new int[]{posx+distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Rechts-Unten
+     public int[] right_low(int distance){
+        if(posx+distance*80 < 800 && posy+distance*80 < 800){
+            diRL();
+            return new int[]{posx+160, posy+80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Oben
+    public int[] straight_up(int distance){
+        if( posy-distance*80 >0){
+            fwd();
+            return new int[]{posy-distance*80};
+        } else{
+            return new int[]{-1};
+        }
+    }
+
+    //Bewegen nach Unten
+    public int[] straight_low(int distance){
+        if( posy+distance*80 >0){
+            bwd();
+            return new int[]{posy+distance*80};
+        } else{
+            return new int[]{-1};
+        }
     }
 
 }
