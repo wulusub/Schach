31c7977974aa2ca158ed5401d7973f282ef8f305
soko003 <138104130+soko003@users.noreply.github.com>
2023-07-13 11:44:30 +0200

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -6,29 +6,95 @@
 
  */
 
-import javax.swing.ImageIcon;
+ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 public class Queen extends King {
     public ImageIcon image;
     public int posx,posy;
-    public boolean set;
-    
+    public JFrame frame;
 
     public Queen(int x , int y, String path, JFrame frame_con){
-        set = false;
+        super(x, y, path, frame_con);
         posx = x;
         posy = y;
-        image = new ImageIcon("images/"+ path +".png");
+        image = new ImageIcon("images/"+ path + ".png");
     }
 
-    public void print(int x, int y, String[][] boardPosition){
-        
-    }
-
+    //King Movement, möglichkeit mehrere Felder zu moven fehlt
     
-  
+    public int fwd() {
+        if (posy-80 > 0) {
+            posy -= 80;
+            return -80;
+        }
+        throw new IllegalArgumentException("Figur kann nicht sich nicht außerhalb des Feldes bewegen!");
 
-    //Bewegen nach Links-Oben
+    }
+
+    public int bwd() {
+        if (posy + 80 < 800) {
+            posy += 80;
+            return 80;
+        }
+        throw new IllegalArgumentException("Figur kann nicht sich nicht außerhalb des Feldes bewegen!");
+    }
+
+    public int swl(){
+        if(posx - 80 > 400){
+            posx -= 80;
+            return -80;
+        }
+        throw new IllegalArgumentException("Figur kann nicht sich nicht außerhalb des Feldes bewegen!");
+    }
+
+    public int swr(){
+        if(posx + 80 < 1200){
+            posx += 80;
+        }
+        throw new IllegalArgumentException("Figur kann nicht sich nicht außerhalb des Feldes bewegen!");
+    }
+
+    public int[] diLU(){
+        try {
+            return new int[]{fwd() , swl()};
+        }
+        catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+        return diLU();
+    }
+
+    public int[] diRU(){
+        try {
+            return new int[]{fwd() , swr()};
+        }
+        catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+        return diRU();
+    }
+
+    public int[] diLL(){
+        try {
+            return new int[]{bwd() , swl()};
+        }
+        catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+        return diLL();
+    }
+
+    public int[] diRL(){
+        try {
+            return new int[]{bwd() , swr()};
+        }
+        catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+        return diRL();
+    }
+
+ //Bewegen nach Links-Oben
     public int[] left_up(int distance){
         if(posx-distance*80 > 400 && posy-distance*80 >0){
             diLU();
