1ed31c08a70083042c72d09e9fca755f55f965f0
JuiRM <138024458+JuiRM@users.noreply.github.com>
2023-07-17 08:01:57 +0200

    Update Pawn.java

--- SCHACH ROMAN HAT ALLES GEMACHT/Pawn.java
+++ SCHACH ROMAN HAT ALLES GEMACHT/Pawn.java
@@ -19,12 +19,14 @@ public class Pawn{
         matrix = matrix_board;
         image = new ImageIcon("images/"+ path + ".png");
     }
+ //Methoden zur Rückgabe der Position
     public int x(){
         return matrix[col][row][0];
     }
     public int y(){
         return matrix[col][row][1];
     }
+ //Methode zur Koordinatenneuzuordnung
     public void changeCords(int colx, int rowy){
         col = colx;
         row = rowy;
